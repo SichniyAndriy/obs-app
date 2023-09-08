796165c7fba4271d1aba7b51f782345ad09bdcb8
Sichniy Andriy <sichniy.andriy@gmail.com>
2023-09-08 17:24:08 +0300

    add main class

---
+++ src/main/java/mate/academy/obsapp/ObsAppApplication.java
@@ -0,0 +1,34 @@
+package mate.academy.obsapp;
+
+import java.math.BigDecimal;
+import mate.academy.obsapp.model.Book;
+import mate.academy.obsapp.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ObsAppApplication {
+    @Autowired
+    private BookService bookService;
+
+    public static void main(String[] args) {
+        SpringApplication.run( ObsAppApplication.class, args );
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setTitle( "bbb" );
+            book.setAuthor( "aaaa" );
+            book.setIsbn( "asdfg" );
+            book.setPrice( BigDecimal.valueOf( 1029 ) );
+            book.setDescription( "qwertyuiop" );
+            book.setCoverImage( "sdsd" );
+            bookService.save( book );
+        };
+    }
+}
