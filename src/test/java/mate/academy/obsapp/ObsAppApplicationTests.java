ec45fcbd4edd818a7c9aa2127f9f649866d9294a
Sichniy Andriy <sichniy.andriy@gmail.com>
2023-09-08 17:16:44 +0300

    add resources in main and in test

---
+++ src/test/java/mate/academy/obsapp/ObsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.obsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
